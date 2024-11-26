3516b1a379d5c2b46174e6fba5dbc19707d9ebda
Artemyev Artur <super-console@mail.ru>
2024-11-26 08:54:04 +0400

    1

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ $("#slider-news").slick({
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
