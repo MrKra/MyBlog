6fd16ddb8ddc504ea417b04311c13777023482bb
MrKra <super.console666@gmail.com>
2024-12-20 10:23:19 +0400

    Add spoiler in form coments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	
+	/**********
+	ON Spoiler
+	**********/
+	$(".spoiler-title").click(function(){
+		if($(".spoiler").hasClass("one")){
+			$(".spoiler-title").not($(this)).removeClass('active');
+			$(".spoiler-text").not($(this).next()).slideUp(500);
+		}
+		$(this).toggleClass('active').next().slideToggle(500);
+	});
+	
 });
